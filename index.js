c24c12bba8e2256f23757a52fd38bfdf0856e551
Caedan <caedanlavender@gmail.com>
2021-07-11 09:05:02 +1200

    Set up endpoint for projects
    Presently using data from users as a placeholder until I get project data

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const mysql = require('mysql2');
 require('dotenv').config();
 
-
+// Declaring local variables with .env details
 const db_host = process.env.DB_HOST
 const db_user = process.env.DB_USER
 const db_password = process.env.DB_PASSWORD
@@ -16,5 +16,32 @@ const db = mysql.createConnection({
 	database	:	'mission_x'
 })
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+
 // Logs if connection is successful
 db.connect(err => console.log(err || "Connection Successful"));
+
+// Project endpoint
+app.get('/projects', (req, res) => {
+	console.log('hit')
+	// console.log(req);
+	db.query('SELECT * FROM users', (err, results) => {
+		if (results.length) {
+			console.log(results);
+			res.status(200).send(results);
+		} else {
+			res.status(400).send("There was an error")
+		}
+	})
+});
+
+// Add your endpoints below
+// i.e:
+// app.get('/something'), (req, res) => blah blah blah
+
+
+
+
+app.listen(4000);
